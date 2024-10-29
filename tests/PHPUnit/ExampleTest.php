e0a7c744c6e890f62844ea1a05a228b62f3e33df
bartek3xe <bartek3xe@gmail.com>
2024-10-29 19:55:06 +0100

    Setup tests

---
+++ tests/PHPUnit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\PHPUnit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasicFunctionality()
+    {
+        $this->assertTrue(true);
+    }
+}
